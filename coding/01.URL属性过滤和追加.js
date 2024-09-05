d1f5520447f8fd33ca618a031e92501c6a00dc10
zhao_jianping <zjp920226@hotmail.com>
2024-09-05 14:42:35 +0800

    feat: URL属性过滤和追加

---
+++ coding/01.URL属性过滤和追加.js
@@ -0,0 +1,45 @@
+/**
+ * @param url 要处理的URL
+ * @param keepParams 要保留的参数列表
+ * @param extraParams 要追加的额外参数
+ * */
+function filterAndAppend(url, keepParams = [], extraParams = {}) {
+  // 解析URL
+  const urlObj = new URL(url);
+  const searchParams = new URLSearchParams(urlObj.search);
+
+  // 过滤参数
+  let filteredParams = {};
+  for (let [key, value] of searchParams) {
+    if (keepParams.includes(key)) {
+      filteredParams[key] = value;
+    }
+  }
+
+  // 将保留的参数重新设置到URL对象上
+  for (let key in filteredParams) {
+    urlObj.searchParams.set(key, filteredParams[key]);
+  }
+
+  // 清除原有的参数
+  urlObj.searchParams.forEach((value, key) => {
+    if (!filteredParams.hasOwnProperty(key)) {
+      urlObj.searchParams.delete(key);
+    }
+  });
+
+  // 追加额外参数
+  for (let key in extraParams) {
+    urlObj.searchParams.append(key, extraParams[key]);
+  }
+
+  // 返回最终的URL
+  return urlObj.href;
+}
+
+// 示例用法:
+const url = 'http://example.com/?a=1&b=2&c=3';
+const keepParams = ['a', 'c'];
+const extraParams = {d: 4};
+
+console.log(filterAndAppend(url, keepParams, extraParams));
